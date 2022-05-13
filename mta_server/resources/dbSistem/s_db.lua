2b21c042f33bdae63565b83345db78e41789cea7
DzIGIBAUER_PC <milosh2255@gmail.com>
2022-05-13 22:09:54 +0200

    Povezivenje ka bazi podataka se sada vrsi pomocu ENV promenljivih u .env fajlu.

--- mta_server/resources/dbSistem/s_db.lua
+++ mta_server/resources/dbSistem/s_db.lua
@@ -2,15 +2,18 @@ local db
 
 function get_connection() return db end
 
--- TODO: koristi .env.
---- Povezuje se sa bazom podataka koristeci 'DB_NAME', 'HOSTNAME', 'USERNAME' i 'PASSWORD',
--- koji treba da budu definisani u scope-u izvan funkcije tj. u 's_config.lua' fajlu.
+--- Povezuje se sa bazom podataka koristeci ENV promenljive koje ucitava pomocu 'ml_sysutils.so' modula.
+-- https://github.com/4O4/mta-ml-sysutils
 -- @return database connection element/nil
 local function povezi_se()
-    db = dbConnect("mysql", "dbname=".. DB_NAME ..";host=".. HOSTNAME ..";charset=utf8", USERNAME, PASSWORD)
+    local db_name = getSystemEnvVariable("DB_NAME")
+    local db_host = getSystemEnvVariable("DB_HOST")
+    local db_user = getSystemEnvVariable("DB_USER")
+    local db_pass = getSystemEnvVariable("DB_PASS")
+    db = dbConnect("mysql", "dbname=".. db_name ..";host=".. db_host ..";charset=utf8", db_user, db_pass)
 
     if not db then
-        iprint("nije uspostavljena veza")
+        iprint("Nije uspostavljena veza ka bazi podataka.")
     end
 
 end
